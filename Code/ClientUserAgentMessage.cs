f28ce17b9e4e55dc241ea5844bf107e6ec3c728a
Edward <v-tazho@hotmail.com>
2018-09-18 14:24:14 +0800

    wcf user agent in core

---
+++ Code/ClientUserAgentMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+
+namespace ConsoleApp2_0
+{
+    internal class ClientUserAgentMessage : IClientMessageInspector
+    {
+        public ClientUserAgentMessage() { }
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            HttpRequestMessageProperty property = new HttpRequestMessageProperty();
+
+            //This is an example user agent that fails, and we would not expect it to.
+            property.Headers["User-Agent"] = "Mozilla /4.0 (compatible; MSIE 6.0; MS Web Services Client Protocol 4.0.30319.42000)";
+            request.Properties.Add(HttpRequestMessageProperty.Name, property);
+
+            return null;
+        }
+    }
+}
---
+++ Code/UserAgentEndpointBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+
+namespace ConsoleApp2_0
+{
+    internal class UserAgentEndpointBehavior : IEndpointBehavior
+    {
+        public UserAgentEndpointBehavior() { }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.ClientMessageInspectors.Add(new ClientUserAgentMessage());
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
